cd97a5619303ddc63a397bd618aa4409d19c136b
Isaac LKM <64147081+Isaaclkm@users.noreply.github.com>
2026-01-04 20:05:59 -0500

    added a lot

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,8 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import CardWrapper from '@/app/ui/dashboard/cards';
+
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -16,12 +18,7 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
